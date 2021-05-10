9648fe6a4bbd74adbc91060b0c25f174ed9fad93
workpebojot <pebojot.edison.work@outlook.com>
2021-05-10 17:25:12 +0800

    Import Footer Component (#1)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img class="img-fluid img-thumbnail" alt="UDMS logo" src="@/assets/logo.png">
     <Overview msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Overview from '@/components/Overview.vue'; // @ is an alias to /src
+import Footer from '@/components/Footer.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     Overview,
+    Footer,
   },
 })
 export default class Home extends Vue {}
