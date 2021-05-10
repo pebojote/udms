ec5fec4fcbeb48ad8a1bc3cc888f2b18d3c928c0
workpebojot <pebojot.edison.work@outlook.com>
2021-05-10 17:25:45 +0800

    Add New Components (#1)

---
+++ src/components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <footer class="text-center text-white" style="background-color: #f1f1f1;">
+            <div class="container p-2">
+                <div class="row gx-1 d-flex justify-content-center">
+                    <div class="col-5 col-sm-3">
+                        <section>
+                            <h6 class="fw-bold text-black-50">
+                                Philippine Main Office
+                            </h6>
+                            <p class="fw-normal text-black-50">
+                                B56 L14 Villaggio Ignatius, Raphael St., Buenavista 1, General Trias, Cavite
+                            </p>
+                            <h6 class="fw-bold text-black-50">
+                                Minnesota, USA Office
+                            </h6>
+                            <p class="fw-normal text-black-50">
+                                10866 101st Place N, Maple Grove, Minnesota, USA.
+                            </p>
+                        </section>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col-7 col-sm-3">
+                        <router-link to="/" class="btn btn-transparent" tabindex="1" role="button" aria-disabled="false">
+                            <font-awesome-icon :icon="['fab', 'facebook-f']" size="lg" />
+                        </router-link>
+                        <router-link to="/" class="btn btn-transparent" tabindex="1" role="button" aria-disabled="false">
+                            <font-awesome-icon :icon="['fab', 'twitter']" size="lg" />
+                        </router-link>
+                        <router-link to="/" class="btn btn-transparent" tabindex="1" role="button" aria-disabled="false">
+                            <font-awesome-icon :icon="['fab', 'instagram']" size="lg" />
+                        </router-link>
+                        <router-link to="/" class="btn btn-transparent" tabindex="1" role="button" aria-disabled="false">
+                            <font-awesome-icon :icon="['fab', 'linkedin']" size="lg" />
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                <font-awesome-icon :icon="['far', 'copyright']" /> 2021 Copyright:
+                <router-link to="/chart">
+                    www.udms.com
+                </router-link>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {}
+</script>
---
+++ src/components/Overview.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1 class="display-1 fw-bold">Upgrade Digital Marketing Services Inc.</h1>
+    <div class="card">
+      <div class="card-body">
+        <p class="lead">
+          Upgrade Digital Marketing Services is a one-stop-shop marketing 
+          and IT solutions company that was established through the humble 
+          beginnings of 3 youngs entrepreneur during COVID-19 pandemic. 
+          Our willingness to provide strong and reliable solutions for 
+          Marketing and IT requirements of every business drives us to start the company.
+        </p>
+      </div>
+    </div>
+    <h1 class="display-3">About Us</h1>
+    <div class="card">
+      <div class="card-body">
+        <p class="lead">
+          Upgrade Digital Marketing Services is a one-stop-shop marketing 
+          and IT solutions company that was established through the humble 
+          beginnings of 3 youngs entrepreneur during COVID-19 pandemic. 
+          Our willingness to provide strong and reliable solutions for 
+          Marketing and IT requirements of every business drives us to start the company.
+        </p>
+      </div>
+    </div>
+    <h1 class="display-3">History</h1>
+    <div class="card">
+      <div class="card-body">
+        <p class="lead">
+          In October 19, 2020, C19 Creative Marketing Solutions was discovered. Then, in November 04, 2020,
+          the business name started as Upgrade Creative Marketing Solutions. And in December 03, 2020,
+          it startup business was registered to Upgrade Digital Marketing Services Inc.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Overview extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
