3b9cea214ba70a95312411634688d4fed03f7c5f
workpebojot <pebojot.edison.work@outlook.com>
2021-05-11 16:46:21 +0800

    Create Organizational Chart (#1)
    I created an organizational chart but this is not
    yet finish so I am giving you a reference to its
    original format. I created it by importing the
    Balkan/OrgChart package.

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1 class="display-1">Chart</h1>
+  <div class="p-2">
+    <div class="mt-1 mb-1"></div>
+    <h1 class="display-1 fw-bold">Chart</h1>
+    <Org />
+    <div class="mt-1 mb-1"></div>
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Org from '@/components/Org.vue'; // @ is an alias to /src
+import Footer from '@/components/Footer.vue';
+
+@Component({
+  components: {
+    Org,
+    Footer
+  },
+})
+export default class Home extends Vue {}
+</script>
