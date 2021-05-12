52579581768db29e0b83aaf5fbbac2093adfa7e2
workpebojot <pebojot.edison.work@outlook.com>
2021-05-12 15:48:12 +0800

    Add New Avatars

--- src/components/Org.vue
+++ src/components/Org.vue
@@ -13,16 +13,16 @@ export default {
     data() {
         return {
                 nodes: [
-                    { id: 1, name: "John Arcel S. Padilla", title: "Chairman and CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
-                    { id: 2, pid: 1, name: "Francisco N. Balan", title: "President and COO", img: "https://cdn.balkan.app/shared/3.jpg" },
-                    { id: 3, pid: 2, name: "Gian Paolo Montano", title: "Consultant", img: "https://cdn.balkan.app/shared/4.jpg" },
-                    { id: 4, pid: 3, name: "Joseph Ledesma", title: "Chief Technology Officer", img: "https://cdn.balkan.app/shared/5.jpg" },
-                    { id: 5, pid: 4, name: "Christian Visaya", title: "Development Director", img: "https://cdn.balkan.app/shared/6.jpg" },
-                    { id: 6, pid: 3, name: "Shaira L. Bautista", title: "VP of Internal Marketing & Communication", img: "https://cdn.balkan.app/shared/7.jpg" },
-                    { id: 7, pid: 3, name: "Fidemar Hernado", title: "VP of Sales and Distribution", img: "https://cdn.balkan.app/shared/8.jpg" },
-                    { id: 8, pid: 6, name: "Grace Manalo", title: "Senior Social Media Expert", img: "https://cdn.balkan.app/shared/8.jpg" },
-                    { id: 9, pid: 6, name: "Sherilyn Ledesma", title: "Creative and Social Media Expert", img: "https://cdn.balkan.app/shared/8.jpg" },
-                    { id: 10, pid: 7, name: "Ferdinand Callueng", title: "Sales Manager", img: "https://cdn.balkan.app/shared/8.jpg" },
+                    { id: 1, name: "John Arcel S. Padilla", title: "Chairman and CEO", img: "https://user-images.githubusercontent.com/38276345/117926120-d9435900-b32a-11eb-8963-95221e0385b1.png" },
+                    { id: 2, pid: 1, name: "Francisco N. Balan", title: "President and COO", img: "https://user-images.githubusercontent.com/38276345/117926134-dcd6e000-b32a-11eb-8aad-1443f5ea138c.png" },
+                    { id: 3, pid: 2, name: "Gian Paolo Montano", title: "Consultant", img: "https://user-images.githubusercontent.com/38276345/117934256-10b70300-b335-11eb-8dde-fac21e98eb77.png" },
+                    { id: 4, pid: 3, name: "Joseph Ledesma", title: "Chief Technology Officer", img: "https://user-images.githubusercontent.com/38276345/117934221-07c63180-b335-11eb-90bc-30789ea1582b.png" },
+                    { id: 5, pid: 4, name: "Christian Visaya", title: "Development Director", img: "https://user-images.githubusercontent.com/38276345/117934245-0dbc1280-b335-11eb-90ce-a758dc758c14.png" },
+                    { id: 6, pid: 3, name: "Shaira L. Bautista", title: "VP of Internal Marketing & Communication", img: "https://user-images.githubusercontent.com/38276345/117926141-df393a00-b32a-11eb-8ff2-fb63f0f473fb.png" },
+                    { id: 7, pid: 3, name: "Fidemar Hernado", title: "VP of Sales and Distribution", img: "https://user-images.githubusercontent.com/38276345/117934239-0bf24f00-b335-11eb-9a74-fa41c048a838.png" },
+                    { id: 8, pid: 6, name: "Grace Manalo", title: "Senior Social Media Expert", img: "https://user-images.githubusercontent.com/38276345/117934265-13b1f380-b335-11eb-8606-ee5b37faa2aa.png" },
+                    { id: 9, pid: 6, name: "Sherilyn Ledesma", title: "Creative and Social Media Expert", img: "https://user-images.githubusercontent.com/38276345/117934268-14e32080-b335-11eb-9bdf-2b2af7a71495.png" },
+                    { id: 10, pid: 7, name: "Ferdinand Callueng", title: "Sales Manager", img: "https://user-images.githubusercontent.com/38276345/117934275-16144d80-b335-11eb-88b7-7c2cc9b59177.png" },
                 ]
             }
         },
