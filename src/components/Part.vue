7a8fec225acfad3613cb605c55c545002f566122
Edison Pebojot <pebojot.edison.work@outlook.com>
2021-06-17 09:54:29 +0800

    Update Part.vue

--- src/components/Part.vue
+++ src/components/Part.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="row p-5">
       <div class="col">
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
           </div>
           <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="1000">
+            <div class="carousel-item active" data-bs-interval="2000">
               <img src="https://user-images.githubusercontent.com/38276345/118633995-a60a3980-b804-11eb-83bc-3a3a29c3c888.png" class="mx-auto d-block img-fluid" alt="...">
               <div class="carousel-caption d-none d-md-block">
                 <h5 class="text-dark">Manuka.AI</h5>
               </div>
             </div>
-            <div class="carousel-item" data-bs-interval="1000">
-              <img src="https://user-images.githubusercontent.com/38276345/122317005-5af05d00-cf4f-11eb-9e1f-2cdd07a01448.png" class="mx-auto d-block img-fluid" alt="...">
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="https://user-images.githubusercontent.com/38276345/122318427-98ee8080-cf51-11eb-8c08-b1db7895ba19.png" class="mx-auto d-block img-fluid" alt="...">
               <div class="carousel-caption d-none d-md-block">
                 <h5 class="text-dark">ProSpark</h5>
               </div>
             </div>
-            <div class="carousel-item" data-bs-interval="1000">
+            <div class="carousel-item" data-bs-interval="2000">
               <img src="https://user-images.githubusercontent.com/38276345/118634036-b0c4ce80-b804-11eb-9339-91d72f0da527.png" class="mx-auto d-block img-fluid" alt="...">
               <div class="carousel-caption d-none d-md-block">
                 <h5 class="text-dark">Neur Media</h5>
@@ -28,12 +28,12 @@
             </div>
           </div>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true" style="color: black"></span>
-            <span class="visually-hidden" style="color: black">Previous</span>
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
           </button>
           <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true" style="color: black"></span>
-            <span class="visually-hidden" style="color: black">Next</span>
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
           </button>
         </div>
       </div>
